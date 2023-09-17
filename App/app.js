d4dbbfb04f2ce927738393f28a3d4f2082dacb15
Sonia Blanco <soniab@timedatasecurity.ie>
2023-09-17 13:05:59 +0100

    using new contrrols

--- App/app.js
+++ App/app.js
@@ -1,5 +1,5 @@
 const { Observable, } = require('rxjs');
-const {filter} = require('rxjs/operators');
+const {filter, share, mapTo} = require('rxjs/operators');
 var mqtt = require('./mqttCluster.js');
 
 global.mtqqLocalPath = process.env.MQTTLOCAL;
@@ -12,16 +12,20 @@ const remoteStream = new Observable(async subscriber => {
     });
   });
 
-  const onStream = remoteStream.pipe(
-    filter( m => m.action==='on')
+  const sharedStream = remoteStream.pipe(share())
+
+  const onStream = sharedStream.pipe(
+    filter( m => m.action==='on'),
+    mapTo("on")
   )
-  const offStream = remoteStream.pipe(
-    filter( m => m.action==='brightness_move_up')
+  const offStream = sharedStream.pipe(
+    filter( m => m.action==='brightness_move_up' || m.action==='brightness_stop'),
+    mapTo("off")
   )
 
   onStream.subscribe(async m => {
-    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee786e7/set',JSON.stringify({state:'ON'}));    
+    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set',JSON.stringify({state:'ON'}));    
   })
   offStream.subscribe(async m => {
-    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee786e7/set',JSON.stringify({state:'OFF'}));    
+    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set',JSON.stringify({state:'OFF'}));    
   })
